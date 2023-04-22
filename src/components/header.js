005ee5d31690bb34c0f5627f07f89781425da8de
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-04-22 16:59:11 +0100

    Implement header as component

---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+
+import styles from './header.module.scss';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className={styles.head}>
+                <div className={styles.title}>
+                    <a className={styles.game} href="https://store.steampowered.com/app/1562430/DREDGE/">
+                        DREDGE
+                    </a>
+                    &nbsp;Item Builder
+                </div>
+                <div className={styles.tag}>
+                    Easily design custom items for
+                    &nbsp;
+                    <a href="https://github.com/Hacktix/Winch">
+                        Winch
+                    </a>
+                </div>
+                <div className={styles.credit}>
+                    built by&nbsp;
+                    <span>
+                        bluey#2108
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
